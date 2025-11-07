28025a8303c9735fbf6563679600d5d52eded6f4
Valerie Racine <vracine21@gmail.com>
2025-11-07 10:35:32 -0800

    fix: ignore large files

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,10 @@
 import { faker } from "@faker-js/faker";
 
+<<<<<<< HEAD
 class User {
+=======
+export class User {
+>>>>>>> b4cd273 (fix: ignore large files)
   name: string;
   location: {
     lat: number;
