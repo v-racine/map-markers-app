63ecd9682c8521e1b14d10ad3202f5834c1653d8
Valerie Racine <vracine21@gmail.com>
2025-11-08 11:37:54 -0800

    initial setup for google maps

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from "./User";
 import { Company } from "./Company";
 
@@ -6,3 +8,11 @@ console.log(user);
 
 const company = new Company();
 console.log(company);
+
+new google.maps.Map(document.getElementById("map")!, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
