dff6d5ddf15163f487e5dc4395061bc9576b2f80
Valerie Racine <116108359+v-racine@users.noreply.github.com>
2025-11-08 12:06:17 -0800

    initial setup for google maps (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from "./User";
 import { Company } from "./Company";
 
@@ -6,3 +8,16 @@ console.log(user);
 
 const company = new Company();
 console.log(company);
+
+const mapDiv = document.getElementById("map");
+if (mapDiv) {
+  new google.maps.Map(mapDiv, {
+    zoom: 1,
+    center: {
+      lat: 0,
+      lng: 0,
+    },
+  });
+} else {
+  console.error("Map element not found.");
+}
